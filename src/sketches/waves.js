76ae7d35fda671b82d86e6738de266be75457ee4
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2021-02-16 17:27:26 +0100

    make waves random colors

--- src/sketches/waves.js
+++ src/sketches/waves.js
@@ -1,14 +1,11 @@
-import canvasSketch from "canvas-sketch";
 import Random from "canvas-sketch-util/random";
 import { linspace } from "canvas-sketch-util/math";
+import random from "canvas-sketch-util/random";
 
-// You can force a specific seed by replacing this with a string value
 const defaultSeed = "";
 
-// Set a random seed so we can reproduce this print later
 Random.setSeed(defaultSeed || Random.getRandomSeed());
 
-// Print to console so we can see which seed is being used and copy it if desired
 console.log("Random Seed:", Random.getSeed());
 
 const settings = {
@@ -23,18 +20,16 @@ const settings = {
 const sketch = ({ width, height }) => {
   const pageSize = Math.min(width, height);
 
-  // page settings
   const margin = 0;
-  const gridSize = 50;
+  const gridSize = 60;
   const background = "black";
+  const strokeHue = random.range(0, 360); 
 
-  // segment settings
   const length = pageSize * 0.1;
   const lineWidth = pageSize * 0.00175;
   const frequency = 0.75;
   const alpha = 1;
 
-  // Create some flat data structure worth of points
   const cells = linspace(gridSize, true)
     .map((v) => {
       return linspace(gridSize, true).map((u) => {
@@ -43,34 +38,27 @@ const sketch = ({ width, height }) => {
     })
     .flat();
 
-  return ({ context, frame, playhead }) => {
-    // Fill the canvas
+  return ({ context, playhead }) => {
     context.fillStyle = background;
     context.globalAlpha = 1;
     context.fillRect(0, 0, width, height);
 
-    // draw grid
     const innerSize = pageSize - margin * 1;
     cells.forEach((cell) => {
       const [u, v] = cell;
 
-      // scale to inner size
       let x = u * innerSize;
       let y = v * innerSize;
 
-      // center on page
       x += (width - innerSize) / 2;
       y += (height - innerSize) / 2;
 
-      // get a random angle from noise
-      const n = Random.noise2D(u * 2 - 1, v * 2 - 1, frequency);
-      // const angle = n * Math.PI * 2;
+      const n = Random.noise2D(u * 2, v * 2, frequency);
       const angle = Math.PI * playhead + n * 5;
 
-      // draw cell
       context.globalAlpha = alpha;
       context.strokeStyle =
-        n < 0.1 ? "transparent" : `hsl(40, 100%, ${n * 100}%)`;
+        n < 0.1 ? "transparent" : `hsl(${strokeHue}, 100%, ${n * 100}%)`;
 
       segment(context, x, y, angle, length, lineWidth);
     });
